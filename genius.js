6e42a45b7b0f724767dff1e6a337e0ce7836608c
tank <35127914+tank0001@users.noreply.github.com>
2020-07-30 14:24:39 -0400

    Update genius.js

--- genius.js
+++ genius.js
@@ -3,11 +3,71 @@ const { token, prefix } = require('./config.json');
 const client = new Discord.Client();
 
 
-
 client.on("message", async message => {
 
   client.user.setPresence({ activity: { name: '$help' }, status: 'online' })
  if(message.author.bot) return;
+
+
+// SUPPORT CHANNELS BLACKLIST
+
+if (msg.startsWith(prefix + "blacklist")) {
+  if (!message.member.hasPermission("MUTE_MEMBERS")) {
+    return message.channel.send("Can't run this command :woozy_face:");
+  }
+  
+  let user = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0]);
+  if (!user) return message.channel.send("Please mention the user.");
+  // Optional:
+  // if (user.id === client.user.id) return message.channel.send("You can't mute me.");
+  // if (user.id === message.author.id) return message.channel.send("You can't mute yourself.");
+  let role = message.guild.roles.cache.find(r => r.name === "SupportBlacklist");
+  let bot = message.guild.members.cache.get(client.user.id).roles.highest;
+  
+  if (!role) return message.channel.send("Couldn't find the mute role.");
+  if (role.position > bot.position) return message.channel.send("The role is higher than me.");
+  
+  let time = args[1];
+  
+  if (!time) {
+    if (user.roles.cache.has(role.id)) return message.channel.send("The user is still muted.");
+    await (user.roles.add(role.id).catch(err => message.channel.send(`Something went wrong: ${err}`)))
+    return message.channel.send(`${user.user.tag} is now muted.`);
+  } else {
+    if (user.roles.cache.has(role.id)) return message.channel.send("The user is still muted.");
+    await (user.roles.add(role.id).catch(err => message.channel.send(`Something went wrong: ${err}`)))
+    
+    let timer = setTimeout(function() {
+      user.roles.remove(role.id).catch(err => message.channel.send(`Something went wrong: ${err}`));
+      message.channel.send(`${user.user.tag} is now unmuted.`);
+    }, ms(time))
+    
+    client.mute.set(user.user.id, timer);
+    message.channel.send(`${user.user.tag} is now muted for **${ms(ms(time), {long: true})}**`);
+  }
+}
+
+if (msg.startsWith(prefix + "whitelist")) {
+  if (!message.member.hasPermission("MUTE_MEMBERS")) {
+    return message.channel.send("You don't have any permissions to do this: Manage Messages/Mute Members/Admin");
+  }
+  
+  let user = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0]);
+  if (!user) return message.channel.send("You need to mention the user.");
+  
+  let role = message.guild.roles.cache.find(r => r.name === "SupportBlacklist");
+  if (!role) return message.channel.send("Couldn't find the mute role.");
+  
+  if (!user.roles.cache.find(r => r.name === "Muted")) return message.channel.send("The user doesn't get muted.");
+  
+  await user.roles.remove(role.id).catch(err => message.channel.send(`Something went wrong: ${err}`));
+  await clearTimeout(client.mute.get(user.user.id));
+  await client.mute.delete(user.user.id);
+  await message.channel.send(`${user.user.tag} is now unmuted.`);
+}
+
+// END SUPPORT CHANNELS BLACKLIST
+
 
 // HELP COMMANDS
 
